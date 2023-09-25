e3e52ef7d5c41b8508416ddc0c2751e2b2e424f1
Aagaard96 <u2455783@rts-365.dk>
2023-09-25 08:28:53 +0200

    dynamic slider length

--- assets/javascript/slider.js
+++ assets/javascript/slider.js
@@ -12,10 +12,11 @@ function changeElement(src, text) {
 fetch('http://localhost:3000/products')
 	.then(res => res.json())
 	.then(data => {
+		console.log(data);
 		FORWARD.addEventListener('click', () => {
 			const currentIndex = Number(IMAGE.dataset.index)
 
-			IMAGE.dataset.index = (currentIndex < 34) ? currentIndex + 1 : 34
+			IMAGE.dataset.index = (currentIndex < data.length-1) ? currentIndex + 1 : data.length-1
 
 			changeElement(data[IMAGE.dataset.index].pictures[0], data[IMAGE.dataset.index].name)
 		})
