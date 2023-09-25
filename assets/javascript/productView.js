fa690e1d7ca8222614b010a2299dbf98aabb6148
Spongey21 <driton.latifi101@gmail.com>
2023-09-25 21:29:29 +0200

    fixed json + dynamic categories and manufacturers

--- assets/javascript/productView.js
+++ assets/javascript/productView.js
@@ -1,5 +1,42 @@
-const ITEMS = document.querySelector('.shop__items--container')
-const MANUFACTURERS = document.querySelector('.shop__manufacturerList')
+const PRODUCTS = document.querySelector('.shop__items--container')
+const MANUFACTURERS = document.querySelector('.shop__shopByMenu')
+const CATEGORIES = document.querySelector('.shop__categoryMenu')
+
+fetch('http://localhost:3000/manufacturers')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(manufacturer => {
+            const ELEMENT = document.createElement('li')
+            ELEMENT.className = 'shop__manufacturer__list'
+
+            ELEMENT.textContent = manufacturer.name
+
+            ELEMENT.addEventListener('click', () => {
+                window.location.href = `?manufacturer=${manufacturer.name}`
+            })
+
+            MANUFACTURERS.append(ELEMENT)
+        })
+    })
+
+fetch('http://localhost:3000/categories')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(category => {
+            const ELEMENT = document.createElement('li')
+            ELEMENT.className = 'shop__categoryItem'
+
+            ELEMENT.textContent = category.name
+
+            ELEMENT.addEventListener('click', () => {
+                window.location.href = `?category=${category.name}`
+            })
+
+            CATEGORIES.append(ELEMENT)
+        })
+    })
+
+const URLPARAMS = new URLSearchParams(window.location.search)
 
 fetch('http://localhost:3000/products')
     .then(res => res.json())
@@ -12,24 +49,29 @@ fetch('http://localhost:3000/products')
                 <a href="#">
                     <img class="shop__items__image" src="${product.pictures[0]}" alt="product image">
                     <h3 class="shop__items__title">${product.name}</h3>
-                    <p class="shop__items__price">${product.price}</p>
+                    <p class="shop__items__price">${product.price}$</p>
                     <button class="shop__items__button">Add to cart</button>
                 </a>
             `
 
-            ITEMS.append(ELEMENT)
-        })
-    })
-
-fetch('http://localhost:3000/manufacturers')
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(manufacturer => {
-            const ELEMENT = document.createElement('li')
-            ELEMENT.className = 'shop__manufacturer__list'
-
-            ELEMENT.textContent = manufacturer.name
-
-            MANUFACTURERS.prepend(ELEMENT)
+            if (URLPARAMS.has('category')) {
+                fetch(product.categories[0].url)
+                    .then(categoriesRes => categoriesRes.json())
+                    .then(categoriesData => {
+                        if (categoriesData.name === URLPARAMS.get('category')) {
+                            PRODUCTS.append(ELEMENT)
+                        }
+                    })
+            } else if (URLPARAMS.has('manufacturer')) {
+                fetch(product.manufacturer[0].link)
+                    .then(manufacturerRes => manufacturerRes.json())
+                    .then(manufacturerdata => {
+                        if (manufacturerdata.name === URLPARAMS.get('manufacturer')) {
+                            PRODUCTS.append(ELEMENT)
+                        }
+                    })
+            } else {
+                PRODUCTS.append(ELEMENT)
+            }
         })
     })
